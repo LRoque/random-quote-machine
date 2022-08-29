13762d67c92da93d9f594f5f6a40858a03b19b03
Lucas Roque <lroque.misc@gmail.com>
2022-08-29 13:46:14 -0300

    basic functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,39 @@
+import React from 'react';
+
 function App() {
-  return <h1>hello world!</h1>;
+  const [refresh, setRefresh] = React.useState(false);
+  const [quote, setQuote] = React.useState({
+    content: '',
+    author: '',
+  });
+
+  React.useEffect(() => {
+    fetch('https://api.quotable.io/random')
+      .then(res => res.json())
+      .then(data => setQuote(data));
+  }, [refresh]);
+
+  function newQuote() {
+    setRefresh(prevValue => !prevValue);
+  }
+
+  return (
+    <div id="quote-box">
+      <p id="text">"{quote.content}"</p>
+      <p id="author">- {quote.author}</p>
+      <div className="links">
+        <a
+          id="tweet-quote"
+          href={`https://twitter.com/intent/tweet?hashtags=quotes&text=${quote.content} -${quote.author}`}
+          target="_blank">
+          TWEET!
+        </a>
+        <button id="new-quote" onClick={newQuote}>
+          New Quote
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
