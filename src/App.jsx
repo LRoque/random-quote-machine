f5c9e6747dfa96c25569afe8d2bca1b6993d2f95
Lucas Roque <lroque.misc@gmail.com>
2022-09-05 06:02:12 -0300

    added color transitions

--- src/App.jsx
+++ src/App.jsx
@@ -6,33 +6,49 @@ function App() {
     content: '',
     author: '',
   });
+  const [color, setColor] = React.useState(randomPastelColor());
 
   React.useEffect(() => {
     fetch('https://api.quotable.io/random')
       .then(res => res.json())
       .then(data => setQuote(data));
+    document.documentElement.style.setProperty('--primary-color', color);
   }, [refresh]);
+
+  function randomPastelColor() {
+    return (
+      'hsl(' +
+      360 * Math.random() +
+      ',' +
+      (25 + 10 * Math.random()) +
+      '%,' +
+      (50 + 10 * Math.random()) +
+      '%)'
+    );
+  }
 
   function newQuote() {
     setRefresh(prevValue => !prevValue);
+    setColor(randomPastelColor);
+    document.documentElement.style.setProperty('--primary-color', color);
   }
 
   return (
     <div id="quote-box">
       {quote.content !== '' && (
         <div>
-          <p id="text">
-            <i class="fa-solid fa-quote-left quotes"></i>
-            {quote.content}
-            <i class="fa-solid fa-quote-right quotes"></i>
-          </p>
+          <div id="text-box">
+            <i className="fa-solid fa-quote-left quotes left-quote"></i>
+            <p id="text">{quote.content}</p>
+            <i className="fa-solid fa-quote-right quotes right-quote"></i>
+          </div>
           <p id="author">- {quote.author}</p>
           <div className="links">
             <a
               id="tweet-quote"
               href={`https://twitter.com/intent/tweet?hashtags=quotes&text=${quote.content} -${quote.author}`}
               target="_blank">
-              <i class="fa-brands fa-twitter"></i>
+              <i className="fa-brands fa-twitter"></i>
             </a>
             <button id="new-quote" onClick={newQuote}>
               New Quote
